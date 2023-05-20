2978dc0092ea3333b16aaaebd092199c25cba1b4
mebiol <97887903+mebiol@users.noreply.github.com>
2023-05-20 20:11:43 +0700

    Add files via upload

---
+++ sort_object.py
@@ -0,0 +1,12 @@
+obj_detected = {'bottle': [110, 30],
+                 'glass': [60, 35],                
+                 'book': [310, 23],
+                 'phone': [90, 33],
+                 'dice': [155, 38],
+             'mouse': [200, 45],
+              'keyboard': [298, 65],
+                'fan': [300, 36]}
+
+obj_detected = sorted(obj_detected.items(), key=lambda x: x[1][0])
+
+print(obj_detected)
